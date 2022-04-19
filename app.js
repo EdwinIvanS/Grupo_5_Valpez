52d8a3ef3252ed7bf0a46d8fc86d5ea7e3cee4f7
EdwinIvanS <sedwin118@hotmail.com>
2022-04-19 17:38:20 -0500

    Agregando imagenes al repositorio y estructurar el  HOME

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(3000, () => {
     console.log("Server running 3000")
 });
 
-app.get("/", (req,res) => {
+app.get("/home", (req,res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
 
