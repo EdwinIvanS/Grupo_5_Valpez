73efcdb120af94ae2aa2bf280b652833f0a0b0e2
EdwinIvanS <sedwin118@hotmail.com>
2022-05-09 20:51:43 -0500

    Vistas dinamicas para los productos nuevos y destacados

--- controllers/principal.js
+++ controllers/principal.js
@@ -1,6 +1,9 @@
+
+const productosPesca = require("../public/javascript/Productos");
+
 const principalController={
     home: function(req,res){
-        res.render("Main/index");
+        res.render("Main/index", {'productosPesca' : productosPesca});
    }
 }
 module.exports = principalController;
