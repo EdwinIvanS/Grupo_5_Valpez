6805f8d4a1e41e066392d340bec97b792c4b4a62
mcamimejia <mcamila.mejia@outlook.com>
2022-05-30 21:52:13 -0500

    modifico procesamiento del register

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const bcryptjs = require('bcryptjs');
+const {validationResult} = require('express-validator');
+
+const usersFilePath = path.join(__dirname, '../Data/usersDataBase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 const usersController={
 
     login: function(req,res){
@@ -13,7 +21,32 @@ const usersController={
     },
 
     userCreate: function(req,res){
+        let validResult = validationResult(req);
+        if(!validResult.isEmpty()){
+            res.render("User/register", {errors: validResult.mapped(), oldData: req.body})
+        }else{
+            let profileImage = req.file;
+            let newUser = {
+                idUser: users.length + 1,
+                name: req.body.name,
+                user: req.body.user,
+                email: req.body.email,
+                dob: req.body.dob,
+                address: req.body.address,
+                photo: "DefaultFish.jpg",
+                password: bcryptjs.hashSync(req.body.password),
+                category: "client"
+            };
+            if (profileImage){
+                newUser.photo = profileImage.filename;
+            }
 
+            users.push(newUser);
+            let usersString = JSON.stringify(users, null, ' ');
+            fs.writeFileSync(usersFilePath, usersString)
+            res.redirect("/users/login")
+            }
+        
     }
 }
 
--- routers/users.js
+++ routers/users.js
@@ -1,15 +1,65 @@
 const express = require('express')
 const router  = express.Router();
+const path = require('path');
+const multer = require('multer');
+const {body} = require('express-validator');
+
+
+//Multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '../public/img/Users'));
+    },
+    filename: function(req, file, cb){
+        cb(null, "img - " + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const uploadFile = multer({storage});
+
+//validator
+
+const validateRegister =[
+    body('name').notEmpty().withMessage("Debes ingresar nombre completo"),
+    body('user')
+        .notEmpty().withMessage("Debes ingresar usuario").bail()
+        .isLength({min: 4, max: 10}).withMessage("El usuario debe contener entre 4 y 10 caracteres"),
+    body('email')
+        .notEmpty().withMessage("Debes ingresar un correo electrónico").bail()
+        .isEmail().withMessage("El correo es inválido"),
+    body('dob').notEmpty().withMessage("Debes ingresar fecha de nacimiento"),
+    body('photo').custom((value, {req})=>{
+        let file = req.file;
+        let acceptedExtensions = [".jpg", ".png", ".gif"];
+        if(file){
+            let fileExtension = path.extname(file.originalname);
+            if(!acceptedExtensions.includes(fileExtension)){
+                throw new Error('Las extensiones del archivo permitidas son: ${acceptedExtensions.join(', ')}')
+            }
+        }
+        return true;
+    }),
+    body('password')
+        .notEmpty().withMessage("Debes ingresar una contraseña").bail()
+        .isAlphanumeric().withMessage("La contraseña debe combinar letras y números")
+        .isLength({min: 6, max: 10}).withMessage("La contraseña debe contener entre 6 y 10 caracteres"),
+    body('confirmPassword').custom((value, {req})=>{
+        if(req.body.password != req.body.confirmPassword){
+            throw new Error("Las contraseñas debe coincidir");
+        }
+        return true;
+    })
+]
 
 // implementar los controladores
 const usersController  = require("../controllers/usersController");
 
 // Enrutador registros
 router.get("/login", usersController.login);
-router.post("./", usersController.logged);
+router.post("/login", usersController.logged);
 
 router.get("/register", usersController.register);
-router.post("./", usersController.userCreate);
+router.post("/register", uploadFile.single('photo'), validateRegister, usersController.userCreate);
 
 
 module.exports=router;
