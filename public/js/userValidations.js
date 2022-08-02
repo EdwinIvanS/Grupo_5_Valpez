091f7b5f47271136a09e2ffeedc80a396ac2a425
mcamimejia <mcamila.mejia@outlook.com>
2022-08-01 21:51:58 -0500

    Validaciones front para forms de register y editUser

--- public/js/userValidations.js
+++ public/js/userValidations.js
@@ -1,15 +1,7 @@
 window.addEventListener("load", function () {
-    let userForm = document.querySelector("form.userForm");
+    let btn = document.querySelector("#btn");
     let errors = {}
     let allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
-
-    let inputName = document.querySelector("#name").value;
-    let inputUser = document.querySelector("#user").value;
-    let inputEmail = document.querySelector("#email").value;
-    let inputDob = document.querySelector("#dob").value;
-    let inputPhoto = document.querySelector("#photo").value;
-    let inputPassword = document.querySelector("#password").value;
-    let inputConfirmPassword = document.querySelector("#confirmPassword").value;
 
     let errorName = document.querySelector("span.errorName");
     let errorUser = document.querySelector("span.errorUser");
@@ -19,15 +11,26 @@ window.addEventListener("load", function () {
     let errorPassword = document.querySelector("span.errorPassword");
     let errorConfirmPassword = document.querySelector("span.errorConfirmPassword");
 
-    userForm.addEventListener("submit", function (e) {
-        
 
+    
+    btn.addEventListener("click", function (e) {
+        let inputName = document.querySelector("#name").value;
+        let inputUser = document.querySelector("#user").value;
+        let inputEmail = document.querySelector("#email").value;
+        let inputDob = document.querySelector("#dob").value;
+        let inputPhoto = document.querySelector("#photo").value;
+        let inputPassword = document.querySelector("#password").value;
+        let inputConfirmPassword = document.querySelector("#confirmPassword").value;
+        console.log(inputName)
         if(inputName == "") {
             errors.name = "Debes ingresar nombre completo";
             errorName.innerHTML = errors.name;
         }else if (inputName.length < 2){
             errors.name = "El nombre debe tener mínimo 2 caracteres";
             errorName.innerHTML = errors.name;
+        }else {
+            delete errors.name;
+            errorName.innerHTML = ""
         }
 
         if(inputUser == ""){
@@ -36,6 +39,9 @@ window.addEventListener("load", function () {
         }else if (inputUser.length < 4){
             errors.user = "El usuario debe contener al menos 4 caracteres";
             errorUser.innerHTML += errors.user;
+        }else {
+            delete errors.user;
+            errorUser.innerHTML = ""
         }
 
         if(inputEmail == ""){
@@ -44,39 +50,49 @@ window.addEventListener("load", function () {
         }else if(!(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}.){1,125}[A-Z]{2,63}$/i.test(inputEmail))){ // .isEmail
             errors.email = "El correo es inválido";
             errorEmail.innerHTML = errors.email;
+        }else {
+            delete errors.email;
+            errorEmail.innerHTML = ""
         }
 
         if(inputDob == ""){
             errors.dob = "Debes ingresar fecha de nacimiento";
             errorDob.innerHTML = errors.dob;
+        }else {
+            delete errors.dob;
+            errorDob.innerHTML = ""
         }
 
-        if(!allowedExtensions.exec(inputPhoto)){
+        if(inputPhoto && !allowedExtensions.exec(inputPhoto)){
             errors.photo = 'Las extensiones del archivo permitidas son: ".jpg", ".png", ".gif", ".jpeg"';
             errorPhoto.innerHTML = errors.photo;
+        }else{
+            delete errors.photo;
+            errorPhoto.innerHTML = ""
         }
 
         if(inputPassword == ""){
             errors.password = "Debes ingresar una contraseña";
             errorPassword.innerHTML = errors.password;
-        }else if (inputPassword){ // .isAlphanumeric
-            errors.password = "La contraseña debe combinar letras y números";
+        }else if (!(/^(?=\w*\d)(?=\w*[a-z])\S{6,10}$/i.test(inputPassword))){ // .isAlphanumeric
+            errors.password = "La contraseña debe combinar letras y números y debe contener entre 6 y 10 caracteres";
             errorPassword.innerHTML = errors.password;
-        }else if (inputPassword.length < 6 || inputPassword.length > 10){
-            errors.password = "La contraseña debe contener entre 6 y 10 caracteres";
-            errorPassword.innerHTML = errors.password;
+        }else{
+            delete errors.password;
+            errorPassword.innerHTML = ""
         }
 
         if(inputConfirmPassword != inputPassword){
-            errors.confirmPassword = "Las contraseñas debe coincidir";
+            errors.confirmPassword = "Las contraseñas deben coincidir";
             errorConfirmPassword.innerHTML = errors.confirmPassword;
+        }else{
+            delete errors.confirmPassword;
+            errorConfirmPassword.innerHTML = ""
         }
 
-        console.log(inputEmail)
         
-        if(Object.keys(errors).length > 1){
+        if(Object.keys(errors).length > 0){
             e.preventDefault();
-            console.log('hola')
         }   
 
     })
